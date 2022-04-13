d6338f3380e99f1785105965c9a7a585ca1bdcfc
Sharath <sharath.esr@gmail.com>
2022-04-14 01:16:00 +0530

    asset: added images for landing page

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import Banner from './images/banner.jpg';
+import Headphones from './images/headphones.jpg';
+import Game from './images/game.jpg';
+import PC from './images/pc.jpg';
+
+export {Banner, Headphones, Game, PC};
