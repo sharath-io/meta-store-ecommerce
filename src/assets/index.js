2a9a21ed58030d488663d538ff708c85d7f99813
Sharath <sharath.esr@gmail.com>
2022-05-02 11:48:33 +0530

    asset: updated images

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,4 +3,8 @@ import Headphones from './images/headphones.jpg';
 import Game from './images/game.jpg';
 import PC from './images/pc.jpg';
 
-export {Banner, Headphones, Game, PC};
+import Controller from './images/controller.jpg';
+import Quest1 from './images/quest1.jpg';
+import Quest2 from './images/quest2.jpg';
+
+export {Banner, Headphones, Game, PC, Controller, Quest1, Quest2};
