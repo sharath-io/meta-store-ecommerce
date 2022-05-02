f2311cd6735d56ce5058be41c0752103937770bf
Sharath <sharath.esr@gmail.com>
2022-05-02 12:04:58 +0530

    feat: added footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import './footer.css';
+
+export default function Footer(){
+return (
+<footer className="footer-class">
+   <p className="description">Made by  ~ @sharath-io</p>
+   <ul>
+            <li className="footer-list">
+               <a className="footer-list-link" href="https://github.com/sharath-io" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a></li>
+            <li className="footer-list">
+               <a className="footer-list-link" href="https://www.linkedin.com/in/sharath-io/" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a></li>
+        </ul>
+</footer>
+)
+}
